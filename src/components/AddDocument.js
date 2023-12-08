006cad7379e07e009ccd24a46a99fe0bba547b02
AKAZOU_Mounia <mounia.akazou@edu.uca.ma>
2023-12-08 13:18:18 +0100

    modification du style d' interface pour ajout des blog

--- src/components/AddDocument.js
+++ src/components/AddDocument.js
@@ -7,24 +7,40 @@ import NavigationBar from './NavigationBar';
 const styles = {
   container: {
     padding: '20px',
-    backgroundColor: '#f5f5dc', // Beige
-    maxWidth: '400px',
+    backgroundColor: 'rgba(255, 248, 225,0.3)', // Beige
+    borderRadius: '10px',
+    boxShadow: '3px 4px 8px rgba(255, 255, 255, 1)',
+    width: '400px',
     margin: 'auto',
+    marginTop:'-50px',
+    height:'400px',
+    borderRadius: '10px',
+    boxShadow: '3px 4px 8px rgba(255, 224, 178, 1)',
+    
   },
   input: {
-    marginBottom: '10px',
+    marginTop:'85px',
     padding: '8px',
     fontSize: '16px',
     width: '100%',
     boxSizing: 'border-box',
+    backgroundColor: 'rgba(255, 248, 225,0.3)', 
+    borderRadius: '30px',
+    border:'3px solid #1B5E20',
+    borderColor:'#1B5E20'
+    
   },
   button: {
     backgroundColor: '#faebd7', // Green
-    color: '#cd853f',
+    color: '#1B5E20',
     padding: '10px',
     fontSize: '16px',
     cursor: 'pointer',
     width: '100%',
+    marginTop:'90px',
+    borderRadius: '30px',
+    border:'3px solid #1B5E20',
+    borderColor:'#1B5E20'
   },
 };
 
